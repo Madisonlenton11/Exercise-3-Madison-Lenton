6810daaba5f2fcc21d4116f5213212ed4cc47117
Madison.Lenton <madison.lenton@uleth.ca>
2023-11-27 18:29:08 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("connected!")
+var x = document.title;
+console.log(x)
+
+window.onload = function() {
+    var imagePaths = [
+        'images/kid-smiling.jpg',
+        'images/kid-cringing.jpg',
+    ];
+}
